df36df560a17c1e934a4866a74193b3155d552f2
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-07-05 13:46:23 +0930

    Move steps to readme

--- calculator.js
+++ calculator.js
@@ -1,16 +1,3 @@
-// Steps for calculator project
-// Display numbers on screen
-// Create button to reset and clear screen
-// Store first number when an operator key is pressed for first time
-// When a second operator or equals key is pressed store second number
-// If equals is pressed after operator key perform calc on number - num1 + num1
-// Repeat last operation every time equals is pressed
-// When two numbers have been entered, calculate result of first and second number
-// Store and display result
-// Add subsequent number to prev result when operator or equals pressed... and so on..
-
-// Reduce if numbers array length === 2
-
 const sum = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
