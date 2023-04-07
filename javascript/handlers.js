788758d3d2af956cfadfc387461566d659dd61c7
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2023-04-07 17:57:24 +0930

    Add numbers and decimal handling

--- javascript/handlers.js
+++ javascript/handlers.js
@@ -1,7 +1,8 @@
 import { calc } from './calc.js';
-import { display } from './main.js';
+import { display, periodBtn } from './main.js';
 
 const memory = {
+  periodCount: 0,
   num1: null,
   num2: null,
   operator: '',
@@ -9,9 +10,48 @@ const memory = {
 };
 
 export const reset = () => {
+  resetPeriod();
   memory.num1 = null;
   memory.num2 = null;
   memory.operator = '';
   memory.result = null;
   display.textContent = '0';
+};
+
+const resetPeriod = () => {
+  memory.periodCount = 0;
+  periodBtn.disabled = false;
+};
+
+export const numberBtnsHandler = (btn) => {
+  if (memory.result) {
+    reset();
+  }
+
+  if (memory.operator) {
+    if (memory.num2) {
+      memory.num2 += btn.id;
+    } else {
+      memory.num2 = btn.id;
+    }
+    display.textContent = memory.num2;
+  } else {
+    if (memory.num1) {
+      memory.num1 += btn.id;
+    } else {
+      memory.num1 = btn.id;
+    }
+    display.textContent = memory.num1;
+  }
+  console.log(memory);
+};
+
+export const periodBtnHandler = (btn) => {
+  memory.periodCount++;
+
+  if (memory.periodCount != 0) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
 };
--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,24 @@
-import { reset } from './handlers.js';
+import { reset, numberBtnsHandler, periodBtnHandler } from './handlers.js';
 
 // DOM Elements
 export const display = document.querySelector('.display > p');
 const onClearBtn = document.querySelector('.button--red');
-const operatorBtns = document.querySelectorAll('.operator');
 const numberBtns = document.querySelectorAll('.number');
+export const periodBtn = document.querySelector('.button__period');
+const operatorBtns = document.querySelectorAll('.operator');
 
 // On/clear button
 onClearBtn.addEventListener('click', reset);
 
+// Number buttons
+numberBtns.forEach((btn) => {
+  btn.addEventListener('click', () => numberBtnsHandler(btn));
+});
+
+// Period button
+periodBtn.addEventListener('click', () => periodBtnHandler(periodBtn));
+
 // // Operator buttons
 // operatorBtns.forEach((btn) => {
 //   btn.addEventListener('click', () => operatorBtnHandler(btn));
-// });
-
-// // Number buttons
-// numberBtns.forEach((btn) => {
-//   btn.addEventListener('click', () => numberBtnsHandler(btn));
 // });
